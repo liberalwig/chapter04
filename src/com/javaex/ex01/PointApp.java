899bcabb65c85924817af0c797188eca2edc0a80
최승은 <liberalfilm@gmail.com>
2021-12-06 11:29:04 +0900

    2021.12.6(월)11:30

--- src/com/javaex/ex01/PointApp.java
+++ src/com/javaex/ex01/PointApp.java
@@ -6,8 +6,12 @@ public class PointApp {
 		Point p01 = new Point(5,7);
 		Point p02 = new Point(10,15);
 		
+		/*
 		System.out.println(p01.toString());
 		System.out.println(p02.toString());
+		*/
+		
+		PointList pList = new PointList();
 	}
 	
 	
---
+++ src/com/javaex/ex01/PointList.java
@@ -0,0 +1,24 @@
+package com.javaex.ex01;
+
+public class PointList {
+
+	// 필드
+	private Point[] pArray;
+	private int crtPos;
+
+	// 생성자
+	public PointList() {
+	}
+
+	/*오히려 이 경우엔 아래와 같이 만들면 위험해짐. 위 생성자는 꼭 써줘야 함.
+	public PointList(Point[] pArray, int crtPos) {
+		this.pArray = pArray;
+		this.crtPos = crtPos;
+	}
+	*/
+	
+	
+	
+	
+	
+}
